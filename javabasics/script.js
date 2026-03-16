2b704c73478cdd3a17e4f2c05137b1d42c408f56
samriddhi johri <sammyjohri04@gmail.com>
2026-03-16 21:58:50 +0530

    commit

---
+++ javabasics/script.js
@@ -0,0 +1,15 @@
+var num=10;
+console.log(num);
+console.log(typeof(num));
+num=false;
+console.log(num);
+console.log(typeof(num));
+num="Sakshi Singh";
+console.log(num);
+console.log(typeof(num));
+num=null;
+console.log(num);
+console.log(typeof(num));
+num=undefined;
+console.log(num);
+console.log(typeof(num));
---
+++ javabasics/script1.js
@@ -0,0 +1,12 @@
+sayHello = function () {
+alert('hello world!');
+}
+sayHello();
+var numberArray = [1, 2, 3];
+var animals = new Array("cat", "dog", "mouse", "lion");
+var person = {
+name: 'Barack Hussein Obama II',
+ age: 51,
+  title: '44th President of the United States'
+}
+console.log(person);
